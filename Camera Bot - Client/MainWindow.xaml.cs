be11154556dab07fb9b2b88c3e5ba45097258706
Garrett Stonis <stonisg@wit.edu>
2021-06-08 00:17:05 -0400

    Preparing client for communication.

--- Camera Bot - Client/MainWindow.xaml.cs
+++ Camera Bot - Client/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Screen = System.Windows.Forms.Screen;
+
 
 namespace Camera_Bot___Client
 {
@@ -21,25 +23,71 @@ namespace Camera_Bot___Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static Color Normal_Color = new Color();
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            
+            Normal_Color.R = 221;
+            Normal_Color.G = 221;
+            Normal_Color.B = 221;
+            Normal_Color.A = 255;
+
+
+            WpfScreen this_screen = WpfScreen.GetScreenFrom(this);
+
+            Top = 0 + (this_screen.WorkingArea.Height / 8);
+            Left = (this_screen.WorkingArea.Width / 2) - (Width / 2);
         }
 
 
         #region Buttons
-
-        private void pin_button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
 
         private void settings_button_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+
+        private void pin_button_Click(object sender, RoutedEventArgs e)
+        {
+            Topmost = !Topmost;
+
+            if (!Topmost)
+            {
+                pin_button.Background = new SolidColorBrush(Normal_Color);
+            }
+            else
+            {
+                pin_button.Background = new SolidColorBrush(Colors.Gray);
+            }
+        }
+
         #endregion Buttons
+
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up:
+                    break;
+
+
+                case Key.Right:
+                    break;
+
+
+                case Key.Down:
+                    break;
+
+
+                case Key.Left:
+                    break;
+            }
+        }
     }
 }
---
+++ Camera Bot - Client/WpfScreen.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+using Screen = System.Windows.Forms.Screen;
+using Point = System.Windows.Point;
+
+namespace Camera_Bot___Client
+{
+    public class WpfScreen
+    {
+        /// <summary>
+        /// Enumerates every screen.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<WpfScreen> AllScreens()
+        {
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                yield return new WpfScreen(screen);
+            }
+        }
+
+        public static WpfScreen GetScreenFrom(Window window)
+        {
+            WindowInteropHelper windowInteropHelper = new WindowInteropHelper(window);
+            Screen screen = Screen.FromHandle(windowInteropHelper.Handle);
+            WpfScreen wpfScreen = new WpfScreen(screen);
+            return wpfScreen;
+        }
+
+        public static WpfScreen GetScreenFrom(Point point)
+        {
+            int x = (int)Math.Round(point.X);
+            int y = (int)Math.Round(point.Y);
+
+            // are x,y device-independent-pixels ??
+            System.Drawing.Point drawingPoint = new System.Drawing.Point(x, y);
+            Screen screen = Screen.FromPoint(drawingPoint);
+            WpfScreen wpfScreen = new WpfScreen(screen);
+
+            return wpfScreen;
+        }
+
+        public static WpfScreen Primary
+        {
+            get { return new WpfScreen(Screen.PrimaryScreen); }
+        }
+
+        private readonly Screen screen;
+
+        internal WpfScreen(Screen screen)
+        {
+            this.screen = screen;
+        }
+
+        public Rect DeviceBounds
+        {
+            get { return GetRect(screen.Bounds); }
+        }
+
+        public Rect WorkingArea
+        {
+            get { return GetRect(screen.WorkingArea); }
+        }
+
+        private Rect GetRect(Rectangle value)
+        {
+            // should x, y, width, height be device-independent-pixels ??
+            return new Rect
+            {
+                X = value.X,
+                Y = value.Y,
+                Width = value.Width,
+                Height = value.Height
+            };
+        }
+
+        public bool IsPrimary
+        {
+            get { return screen.Primary; }
+        }
+
+        public string DeviceName
+        {
+            get { return screen.DeviceName; }
+        }
+    }
+}
